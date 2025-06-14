c87c5422771acce269b4b6543c579e8956b72f7e
MaicolCode <paucarjhon07@outlook.com>
2025-06-13 22:46:14 -0500

    Refactor Navbar component to improve context usage and streamline sign-out functionality

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -4,112 +4,110 @@ import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
-  const context = useContext(ShoppingCartContext)
+  const { signOut, setSignOut, setSearchByCategory, cartProducts } =
+    useContext(ShoppingCartContext)
+
+  // Function to change a state to SignOut
+  const changeStateSignOut = () => {
+    localStorage.setItem('sign-out', true)
+    setSignOut(true)
+  }
+
+  console.log(signOut)
+
   const activeStyle = 'underline underline-offset-4'
 
   return (
     <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
-          <NavLink to='/'>
-            Shopi
-          </NavLink>
+          <NavLink to='/'>Shopi</NavLink>
         </li>
         <li>
           <NavLink
             to='/'
-            onClick={() => context.setSearchByCategory()}
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            onClick={() => setSearchByCategory()}
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             All
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/clothes'
-            onClick={() => context.setSearchByCategory('clothes')}
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            onClick={() => setSearchByCategory('clothes')}
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Clothes
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/electronics'
-            onClick={() => context.setSearchByCategory('electronics')}
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            onClick={() => setSearchByCategory('electronics')}
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Electronics
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/furnitures'
-            onClick={() => context.setSearchByCategory('furnitures')}
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            onClick={() => setSearchByCategory('furnitures')}
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Furnitures
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/toys'
-            onClick={() => context.setSearchByCategory('toys')}
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            onClick={() => setSearchByCategory('toys')}
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Toys
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/others'
-            onClick={() => context.setSearchByCategory('others')}
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            onClick={() => setSearchByCategory('others')}
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Others
           </NavLink>
         </li>
       </ul>
       <ul className='flex items-center gap-3'>
-        <li className='text-black/60'>
-          teff@platzi.com
-        </li>
+        <li className='text-black/60'>teff@platzi.com</li>
         <li>
           <NavLink
             to='/my-orders'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             My Orders
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/my-account'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             My Account
           </NavLink>
         </li>
         <li>
           <NavLink
             to='/sing-in'
-            className={({ isActive }) =>
-              isActive ? activeStyle : undefined
-            }>
-            Sign In
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+            onClick={() => changeStateSignOut()}
+          >
+            Sign Out
           </NavLink>
         </li>
         <li className='flex items-center'>
           <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
-          <div>{context.cartProducts.length}</div>
+          <div>{cartProducts.length}</div>
         </li>
       </ul>
     </nav>
