c17e676e46658c9b00254c0a9b92fcda711f54b5
MaicolCode <paucarjhon07@outlook.com>
2025-07-20 19:11:36 -0500

    Refactor CheckoutSideMenu component for improved readability and maintainability

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -10,7 +10,9 @@ const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
 
   const handleDelete = (id) => {
-    const filteredProducts = context.cartProducts.filter(product => product.id != id)
+    const filteredProducts = context.cartProducts.filter(
+      (product) => product.id != id
+    )
     context.setCartProducts(filteredProducts)
   }
 
@@ -27,38 +29,52 @@ const CheckoutSideMenu = () => {
     context.setSearchByTitle(null)
   }
 
+  const isProductsInCart = context.cartProducts.length > 0
+
   return (
     <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+      className={`${
+        context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'
+      } checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>My Order</h2>
         <div>
           <XMarkIcon
             className='h-6 w-6 text-black cursor-pointer'
-            onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
+            onClick={() => context.closeCheckoutSideMenu()}
+          ></XMarkIcon>
         </div>
       </div>
       <div className='px-6 overflow-y-scroll flex-1'>
-        {
-          context.cartProducts.map(product => (
-            <OrderCard
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              imageUrl={product.images}
-              price={product.price}
-              handleDelete={handleDelete}
-            />
-          ))
-        }
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+            handleDelete={handleDelete}
+          />
+        ))}
       </div>
       <div className='px-6 mb-6'>
         <p className='flex justify-between items-center mb-2'>
           <span className='font-light'>Total:</span>
-          <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+          <span className='font-medium text-2xl'>
+            ${totalPrice(context.cartProducts)}
+          </span>
         </p>
         <Link to='/my-orders/last'>
-          <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
+          <button
+            className={`bg-black py-3 text-white w-full rounded-lg ${
+              !isProductsInCart ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            onClick={() => handleCheckout()}
+            disabled={!isProductsInCart}
+          >
+            Checkout
+          </button>
         </Link>
       </div>
     </aside>
